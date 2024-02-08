993a5cd6dd2c14032f3ccf35f2dbe3da430b1e76
otavio dias <otaviohenriqueerm@gmail.com>
2024-02-07 22:40:47 -0300

    mudancas no layout da tela

--- tela.py
+++ tela.py
@@ -1,28 +1,28 @@
 from flet import *
 
 class Cadastro(UserControl):
-    def __init__(self):
-        pass
+    def __init__(self, page: Page):
+        self.page = page
     
     def build(self):
-        self.botao_adicionar_pessoa = TextButton(on_click=None, content=Row([Text("Adicionar Pessoa", text_align=TextAlign.CENTER)], alignment=CrossAxisAlignment.CENTER))
-        self.botao_buscar_todas_pessoa = TextButton(text="Buscar Todas as Pessoa", on_click=None)
-        self.botao_buscar_pessoa_email = TextButton(text="Buscar Pessoa por Email", on_click=None)
-        self.botao_alterar_dados_pessoa = TextButton(text="Alterar Dados da Pessoa", on_click=None)
-        self.botao_excluir_pessoa = TextButton(on_click=None, content=Row([Text("Excluir Pessoa", text_align=TextAlign.CENTER)]))
+
+        self.page.navigation_bar = NavigationBar(
+            destinations=[
+                NavigationDestination(label='Adicionar Pessoa'),
+                NavigationDestination(label='Buscar Todas as Pessoa'),
+                NavigationDestination(label='Buscar Pessoa por Email'),
+                NavigationDestination(label='Alterar Dados da Pessoa'),
+                NavigationDestination(label='Excluir Pessoa'),
+            ]
+        )
 
         self.container_botoes = Container(
             content=Column(
                 controls=[
-                    self.botao_adicionar_pessoa,
-                    self.botao_buscar_todas_pessoa,
-                    self.botao_buscar_pessoa_email,
-                    self.botao_alterar_dados_pessoa,
-                    self.botao_excluir_pessoa
+                   Text("Hello", text_align=TextAlign.CENTER)
                 ], horizontal_alignment=MainAxisAlignment.CENTER
             ),
-            bgcolor=colors.AMBER_100,
-            width=300
+            bgcolor=colors.AMBER_100
         )
 
         self.container_informacoes = Container(
@@ -30,7 +30,7 @@ class Cadastro(UserControl):
                 controls=[
                     self.container_botoes
                 ]
-            ),alignment=alignment.center
+            ), alignment=alignment.center
         )
 
         return self.container_informacoes
@@ -38,7 +38,8 @@ class Cadastro(UserControl):
 def main(page: Page):
     page.title = "Registro"
     page.theme_mode = ThemeMode.LIGHT
-    cadastro = Cadastro()
+    
+    cadastro = Cadastro(page)
     cadastro.build()
     page.add(
         cadastro.build()
